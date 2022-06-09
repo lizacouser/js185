d8f9f4ffeaea925a45c059eea599e58dcd83dadf
lizacouser <liza.couser.95@gmail.com>
2022-06-09 14:32:27 -0400

    films and expenses projects

---
+++ films_project/app.js
@@ -0,0 +1,12 @@
+const { Client } = new require('pg');
+let client = new Client({ database: 'films' });
+
+async function logQuery(queryText) {
+  await client.connect();
+  let data = await client.query(queryText);
+
+  console.log(data.rows[2].count);
+  client.end();
+}
+
+logQuery("SELECT count(id) FROM films WHERE duration < 110 GROUP BY genre;");
